389c1257d64dd351171275f5d09653396055de95
leahgwin <leahgwin@gmail.com>
2018-08-31 14:14:12 -0500

    looped through songs that werent nickelback and added them to good song list. Then console writeline them

--- Program.cs
+++ Program.cs
@@ -7,10 +7,7 @@ namespace nickelback
     {
         static void Main(string[] args)
         {
-            // Make sure that some of the songs are from the band Nickelback.You can see a list of their greatest hits on Amazon.
-            // Once the set is populated with 7 tuples, iterate over the set of songs, and check if the band name is "Nickelback".
-            // If the band is not Nickelback, then add it to the goodSongs list.
-            // USe another foreach loop to print out all the good songs.
+
 
             // Define a List, named goodSongs, that will hold tuples consisting of two strings.
             List<(string item, string item2)> goodSongs = new List<(string, string)>();
@@ -18,21 +15,32 @@ namespace nickelback
             // Define a HashSet, named allSongs, that contains 7 tuples.Each tuple should contain two string values:
             // The name of an artist
             // A song by that artist
-            HashSet<(string, string)> allSongs = new HashSet<(string, string)>()
+            HashSet<(string songName, string artistName)> allSongs = new HashSet<(string, string)>()
             {
           ("Photograph", "Nickelback"),
           ("Far Away", "Nickelback"),
-          ("Should've Said No", "Taylor Swift"),
+          ("Dress", "Taylor Swift"),
           ("Over", "Thirdstory"),
           ("This is America", "Childish Gambino"),
           ("If Today Was Your Last Day", "Nickelback"),
           ("Sure Thing", "Miguel")
-
             };
 
+            // Once the set is populated with 7 tuples, iterate over the set of songs, and check if the band name is "Nickelback".
+            // If the band is not Nickelback, then add it to the goodSongs list.
 
-
-
+            foreach ((string songName, string artistName) song in allSongs)
+            {
+                if (song.artistName != "Nickelback")
+                {
+                    goodSongs.Add((song));
+                }
+            };
+            // USe another foreach loop to print out all the good songs.
+            foreach ((string songName, string artistName) song in goodSongs)
+            {
+                Console.WriteLine($"I really like the song, {song.songName}, by {song.artistName}!");
+            }
 
         }
     }
